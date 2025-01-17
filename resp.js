0dad21346d41caded3aa9b52225d63bfc83d7315
AzRafa <glowhithy@outlook.com>
2025-01-17 10:21:26 -0300

    responsitvity update

--- resp.js
+++ resp.js
@@ -1,19 +1,10 @@
 const header = document.querySelector('header');
 const headerManager = document.querySelector('.header-manager');
-const logo = document.querySelector('#logo');
 const sections = document.querySelectorAll('section');
 const logos = document.querySelector('.logos');
 
 headerManager.addEventListener('click', () => {
   header.classList.toggle('header-open');
-});
-
-window.addEventListener('resize', () => {
-  if (window.innerWidth < 720) {
-    logo.innerText = '</>';
-  } else {
-    logo.innerText = '</> Fullstack Developer';
-  }
 });
 
 header.querySelectorAll('nav').forEach((nav, index) => {
@@ -25,6 +16,7 @@ header.querySelectorAll('nav').forEach((nav, index) => {
         top: sectionPosition - offset,
         behavior: 'smooth'
       });
+      header.classList.toggle('header-open');
     }
   });
 });
@@ -33,6 +25,7 @@ let index = 0;
 
 function logoMoviment(logo) {
   index < logos.children.length - 1 ? index ++ : index = 0;
+  logo.classList.contains('up') ? logo.classList.remove('up') : logo.classList.add('up');
 
   setTimeout(() => {
     logo.classList.contains('up') ? logo.classList.remove('up') : logo.classList.add('up');
